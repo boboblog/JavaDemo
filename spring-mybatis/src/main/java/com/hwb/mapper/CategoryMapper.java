7ae70670612dc6c7f9d58d8704c2cf5e4a560d38
黄伟波 <huangwb9@yonyou.com>
2019-11-08 17:01:35 +0800

    spring-mvc项目初始化

---
+++ spring-mybatis/src/main/java/com/hwb/mapper/CategoryMapper.java
@@ -0,0 +1,21 @@
+package com.hwb.mapper;
+
+import java.util.List;
+
+import com.hwb.pojo.Category;
+
+public interface CategoryMapper {
+	  
+    public int add(Category category); 
+        
+    public void delete(int id); 
+        
+    public Category get(int id); 
+      
+    public int update(Category category);  
+        
+    public List<Category> list();
+     
+    public int count(); 
+     
+}
